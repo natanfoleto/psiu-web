1d3da0265ebd5e7f963d82bb290005ec3fc11b1a
Natan Foleto <natanfoleto2015@hotmail.com>
2024-11-07 22:26:51 -0300

    Update 2 07/11/2024

--- src/pages/home/new-post/index.tsx
+++ src/pages/home/new-post/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 
+import { Button } from '@/components/button'
+
 import { ButtonNewPost } from './button-new-post'
 
 export function NewPost() {
   const [open, setOpen] = useState(false)
+  const [content, setContent] = useState('')
 
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
@@ -21,19 +24,37 @@ export function NewPost() {
             onClick={(e) => e.stopPropagation()}
             className="w-[576px] h-96 rounded-lg bg-zinc-800"
           >
-            <div className="bg-zinc-900 p-2 rounded-t-lg">
+            <div className="bg-zinc-900 p-3 rounded-t-lg">
               <h1 className="text-zinc-300 text-sm font-semibold text-center">
                 Criar nova publicação
               </h1>
             </div>
 
-            <form className="flex flex-col">
+            <form className="h-full flex flex-col justify-between bg-zinc-800 rounded-lg">
               <textarea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
                 placeholder="Sobre o que você quer falar?"
-                className="flex-1"
+                className="
+                  flex-1 
+                  bg-transparent 
+                  outline-0 
+                  text-sm 
+                  text-zinc-200 
+                  border-b-[1px] 
+                  border-zinc-700
+                  p-3
+                "
               ></textarea>
 
-              <button>Postar</button>
+              <div className="flex justify-end py-4 px-3">
+                <Button
+                  className="bg-zinc-900 disabled:bg-zinc-700 disabled:cursor-not-allowed"
+                  disabled={!content}
+                >
+                  Publicar
+                </Button>
+              </div>
             </form>
           </div>
         </div>
