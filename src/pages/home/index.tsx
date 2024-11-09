858bee2a71a52c70dec1175732cf8196c5364157
Natan Foleto <natanfoleto2015@hotmail.com>
2024-11-08 21:36:14 -0300

    Update 08/11/2024

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,34 @@
+import { Post } from '@/components/post'
+
 import { NewPost } from './new-post'
 
 export function Home() {
   return (
-    <div className="px-16 py-8">
+    <div className="h-screen w-full px-16 py-8 space-y-12 overflow-y-auto">
       <NewPost />
+
+      <div className="flex flex-col items-center gap-12">
+        <Post
+          id=""
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum obcaecati quis magni quidem modi aliquam voluptate id veritatis aut. Eligendi voluptas quidem fugiat dolores recusandae vitae esse dolor quos velit!"
+          publishedAt="2024-11-08T23:57:50.054Z"
+          updatedAt={null}
+        />
+
+        <Post
+          id=""
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eaque, odio quidem laborum quam eum mollitia ducimus rem ad perspiciatis fugit aspernatur eligendi, repudiandae beatae voluptatem error, doloribus porro sunt."
+          publishedAt="2024-10-08T23:57:50.054Z"
+          updatedAt={null}
+        />
+
+        <Post
+          id=""
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laboriosam consectetur rem vel, quos accusamus adipisci iure explicabo tenetur maiores. Cupiditate deserunt, doloremque atque minus ab minima eius rerum quia."
+          publishedAt="2024-09-08T23:57:50.054Z"
+          updatedAt="2024-11-08T18:00:50.054Z"
+        />
+      </div>
     </div>
   )
 }
