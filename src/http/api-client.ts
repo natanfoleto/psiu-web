c10c1f4c6e33a66ad9c45fe4c95970a448507c49
Natan Foleto <natanfoleto2015@hotmail.com>
2025-01-06 20:30:06 -0300

    Fixing error 401 logout

--- src/http/api-client.ts
+++ src/http/api-client.ts
@@ -1,4 +1,4 @@
-import ky, { BeforeRequestHook } from 'ky'
+import ky, { type AfterResponseHook, BeforeRequestHook } from 'ky'
 
 const prefixUrl = 'http://localhost:3333'
 
@@ -17,10 +17,24 @@ const beforeRequest: BeforeRequestHook = async (_request) => {
   if (token) _request.headers.set('Authorization', `Bearer ${token}`)
 }
 
+const afterResponse: AfterResponseHook = async (
+  _request,
+  _options,
+  response,
+) => {
+  if (response.status === 401) {
+    localStorage.clear()
+    sessionStorage.clear()
+
+    window.location.href = '/'
+  }
+}
+
 export const api = ky.create({
   prefixUrl,
   hooks: {
     beforeRequest: [beforeRequest],
+    afterResponse: [afterResponse],
   },
   credentials: 'include',
 })
